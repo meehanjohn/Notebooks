5c15639d27ee62a8e5c64a935b617a6835e12476
meehanjohn <meehan6@illinois.edu>
2019-07-14 08:50:44 -0400

    Project 4 files

---
+++ Data Analyst Nanodegree Notes/Project 4 - Wrangle and Analyze Data/creds.py
@@ -0,0 +1,4 @@
+consumer_key = "6KNN0qHIuy6EtdIEax2Rb0Z9G"
+consumer_secret = 'vJlUbMXgjCWb0qQAZX0mRCAYRdU3qsl71uOFD8qBMAAPnHW3jQ'
+access_token = '1147697390155157510-i9JIc5m5xdGCYvOVQBYaw1XWeZPbAX'
+access_secret = 'rBmTf2uFLWzYAdgzt0ZmvrdqUBh6YYmjHq6ihOrHp1Y7B'
